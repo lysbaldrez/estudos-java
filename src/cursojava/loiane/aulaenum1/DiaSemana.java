dfd857a8e9c7908d927a95fb7ae908fd21aec5a6
lysbaldrez <lyscalefi@gmail.com>
2020-09-02 19:07:02 -0400

    Eu vou ficar muito boa nisso

---
+++ src/cursojava/loiane/aulaenum1/DiaSemana.java
@@ -0,0 +1,6 @@
+package cursojava.loiane.aulaenum1;
+
+public enum DiaSemana {
+
+    SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO;
+}
---
+++ src/cursojava/loiane/aulaenum1/DiaSemanaConstantes.java
@@ -0,0 +1,14 @@
+package cursojava.loiane.aulaenum1;
+
+public class DiaSemanaConstantes {
+
+    //Quando se declara constante, é em caixa alta, e separar palavra com _ 
+    // O public static serve para facilitar o uso em outra classe, ai não precisa instanciar esse valor 
+    public static final int SEGUNDA = 1;
+    public static final int TERCA = 2;
+    public static final int QUARTA = 3;
+    public static final int QUINTA = 4;
+    public static final int SEXTA = 5;
+    public static final int SABADO = 6;
+    public static final int DOMINGO = 7;
+}
